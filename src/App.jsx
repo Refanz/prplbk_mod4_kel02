4d25f5382ec2d9988452cd0643278101268d5360
Khasandranur <khasandranur@gmail.com>
2023-10-04 22:28:29 +0700

    Menambahkan filter pencarian

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,71 @@
-import {useEffect, useState} from 'react'
-import './App.css'
-import {Avatar, List, ListItem, ListItemAvatar, ListItemText, TextField, Typography} from "@mui/material";
-import {Person} from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import "./App.css";
+import {
+  Avatar,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Person } from "@mui/icons-material";
 
 function App() {
+  const [users, setUsers] = useState([
+    "Khasandra",
+    "Didan",
+    "Refan",
+    "Baihaqi",
+  ]);
 
-    const [users, setUsers] = useState(["Khasandra", "Didan", "Refan", "Baihaqi"]);
+  const [searchUser, setSearchUser] = useState("");
 
-    const [searchUser, setSearchUser] = useState("");
-    
-    function searchUserByName(event) {
-      setSearchUser(event.target.value);
+  function searchUserByName(event) {
+    setSearchUser(event.target.value);
 
-      console.log(searchUser);
+    console.log(searchUser);
   }
-    return (
-        <>
-            <div>
-            <Typography variant="h4">
-                    Tugas Modul 4 Kelompok 2
-                </Typography>
+  return (
+    <>
+      <div>
+        <Typography variant="h4">Tugas Modul 4 Kelompok 2</Typography>
 
-                <TextField id="outlined-basic" label="Cari user.." variant="outlined" size="small" margin="normal"
-                           onChange={searchUserByName}/>
+        <TextField
+          id="outlined-basic"
+          label="Cari user.."
+          variant="outlined"
+          size="small"
+          margin="normal"
+          onChange={searchUserByName}
+        />
 
-                <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper', marginY: 3}}>
-                    </List>
-            </div>
-
-        </>
-    )
+        <List
+          sx={{
+            width: "100%",
+            maxWidth: 360,
+            bgcolor: "background.paper",
+            marginY: 3,
+          }}
+        >
+          {users
+            .filter((user) =>
+              user.toLowerCase().match(searchUser.toLowerCase(), "i")
+            )
+            .map((item, index) => (
+              <ListItem key={index}>
+                <ListItemAvatar>
+                  <Avatar>
+                    <Person />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={item} />
+              </ListItem>
+            ))}
+        </List>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
